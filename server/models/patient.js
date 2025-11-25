ce955a41b57ef1fcc4676b4155171653671c64c6
sanjitpoiu <149330492+sanjitpoiu@users.noreply.github.com>
2025-11-25 12:12:09 +0530

    Update patient schema to include phone and timestamps
    Replaced 'age' with 'phone' in patient schema and added timestamps.

--- server/models/patient.js
+++ server/models/patient.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
 const patientSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  age: { type: Number, required: true },
-  gender: { type: String, required: true },
-});
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phone: { type: String, required: true }
+}, { timestamps: true });
 
-const Patient = mongoose.model('Patient', patientSchema);
-
-module.exports = Patient;
+module.exports = mongoose.model('Patient', patientSchema);
